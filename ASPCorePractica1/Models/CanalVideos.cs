3de8b82cdc6c14ae3a17b3f1bfeb5813e6420579
Posada-rgb <72574472+Posada-rgb@users.noreply.github.com>
2020-10-09 16:47:08 -0500

    se añadieron nuevos archivos al repos

---
+++ ASPCorePractica1/Models/CanalVideos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCorePractica1.Models
+{
+    public class CanalVideos
+    {
+        public string   IDA                     { get; set; }
+        public string   Nick_userA              { get; set; }
+        public string   Lista_RepA              { get; set; }
+        public string   CategoriaVid_1A         { get; set; }
+        public string   CategoriaVid_2A         { get; set; }
+        public string   CategoriaVid_3A         { get; set; }
+        public string   Mas_vistoA              { get; set; }
+        public string   Descripcion_canalA      { get; set; }
+        public DateTime Fecha_creacionA         { get; set; }
+
+        public string   IDB                     { get; set; }
+        public string   Nick_userB              { get; set; }
+        public string   Lista_RepB              { get; set; }
+        public string   CategoriaVid_1B         { get; set; }
+        public string   CategoriaVid_2B         { get; set; }
+        public string   CategoriaVid_3B         { get; set; }
+        public string   Mas_vistoB              { get; set; }
+        public string   Descripcion_canalB      { get; set; }
+        public DateTime Fecha_creacionB         { get; set; }
+
+        public string   IDC                     { get; set; }
+        public string   Nick_userC              { get; set; }
+        public string   Lista_RepC              { get; set; }
+        public string   CategoriaVid_1C         { get; set; }
+        public string   CategoriaVid_2C         { get; set; }
+        public string   CategoriaVid_3C         { get; set; }
+        public string   Mas_vistoC              { get; set; }
+        public string   Descripcion_canalC      { get; set; }
+        public DateTime Fecha_creacionC         { get; set; }
+
+    }
+}
